aab968b4f92c6113a5cc9223f4f84fd8051c3784
Binalayefa <binaziki@gmai.com>
2025-02-22 03:21:00 +0000

    Added WelcomeMessage component and updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,6 @@
+
 import { useState } from 'react'
+import WelcomeMessage from './components/WelcomeMessage';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,6 +10,9 @@ function App() {
 
   return (
     <>
+    <div>
+            <WelcomeMessage />  {/* Use the component inside the return statement */}
+        </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

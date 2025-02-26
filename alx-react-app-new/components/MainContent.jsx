b8c57891830e902acbeb19cc1ee6fb6794a17f26
Binalayefa <binaziki@gmai.com>
2025-02-26 21:11:04 +0000

    Fixed inline styles for all components

--- alx-react-app-new/components/MainContent.jsx
+++ alx-react-app-new/components/MainContent.jsx
@@ -1,10 +1,10 @@
 function MainContent() {
-  return (
-    <main style={{ backgroundColor: '#f4f4f4', padding: '20px', textAlign: 'center' }}>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
-    </main>
-  );
-}
-
-export default MainContent;
-
+    return (
+      <main style={{ backgroundColor: '#f4f4f4', padding: '20px', textAlign: 'center' }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    );
+  }
+  
+  export default MainContent;
+  

9db0b95f0b515f5c92767e3b9e728b2e59a902b1
Binalayefa <binaziki@gmai.com>
2025-03-28 18:38:19 +0000

    Fixed Tailwind content configuration

--- tailwind-react-integration/tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind-react-integration/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Updated
-  theme: {
-    extend: {},
-  },
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: "media", // or "class"
+  theme: { extend: {} },
+  variants: { extend: {} },
   plugins: [],
 };

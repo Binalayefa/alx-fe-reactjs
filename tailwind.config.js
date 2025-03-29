e1945558d5740184fed7a70137acf0de823f6d50
Binalayefa <binaziki@gmai.com>
2025-03-29 01:28:03 +0000

    Fixed Tailwind CSS configuration issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Make sure these paths match your project structure
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
 
 

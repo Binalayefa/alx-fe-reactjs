423689424e66cd77f31a6037f26d2891aae47bf8
Binalayefa <binaziki@gmai.com>
2025-03-31 05:20:19 +0100

    Removed tailwind-react-integration directory and updated package-lock.json

--- tailwind-react-integration/tailwind-react-integration/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: "media", // or "class"
-  theme: { extend: {} },
-  variants: { extend: {} },
-  plugins: [],
-};
